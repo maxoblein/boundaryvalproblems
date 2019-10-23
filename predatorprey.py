454ecf18bfa0e322e3cdf1ed814591f13d7f6619
maxoblein <max.oblein@gmail.com>
2019-10-23 12:04:40 +0100

    adding functionality to plot solution to ode

--- predatorprey.py
+++ predatorprey.py
@@ -1,33 +1,48 @@
+import sys
+from scipy.integrate import ode
+import matplotlib.pyplot as plt
+import numpy as np
 def odefuncPP(X,t,parameters):
     a = parameters[0]
     b = parameters[1]
     d = parameters[2]
     dx = X[0]*(1-X[0]) - ((a*X[0]*X[1])/(d+X[0]))
     dy = b * X[1] * (1 - (X[1]/X[0]))
-    dXdt = [dx dy]
-    return dXdt
+    dXdt = [dx,dy]
+    return np.array(dXdt)
 
 # Function to implement one step of the runge kutter fourth order method
 # X1 is the x at start of step and X2 is the end with respective t1 and t2
-def rk4(f,X1,t1,t2):
+def rk4(f,X1,t1,t2,parameters):
 
     h = t2-t1
-    k1 = h*f(X1,t1)
-    k2 = h*f(X1+(k1/2),t1+(h/2))
-    k3 = h*f(X1+(k2/2),t1+(h/2))
-    k4 = h*f(X1+k3,t1+h)
+    k1 = h*f(X1,t1,parameters)
+    k2 = h*f(X1+(k1/2),t1+(h/2),parameters)
+    k3 = h*f(X1+(k2/2),t1+(h/2),parameters)
+    k4 = h*f(X1+k3,t1+h,parameters)
     X2 = X1 + ((1/6) * (k1+(2*k2)+(2*k3)+k4))
-
     return X2
 
-def rk4solver(f,X0,t):
+def rk4solver(f,X0,t,parameters):
     #decide on a step size
     h = 0.1
     t1 = t[0]
+    rk4_plot = []
     X_rk4 = X0
     #check that rk4 does not go pat the tend specified
     while t1 <= t[-1]:
         tnext = min(t[-1],t1+h)
-        X_rk4 = rk4(odefunc,X_rk4,t,tnext)
+        X_rk4 = rk4(f,X_rk4,t1,tnext,parameters)
+        print(np.size(X_rk4))
         t1 = t1+h
-    return X_rk4
+    return rk4_plot
+
+if __name__ == '__main__':
+    X0 = [0.4,0.5]
+    t = np.linspace(0,5)
+    parameters = [1,0.1,0.3]
+    plot_array = rk4solver(odefuncPP,X0,t,parameters)
+    '''fig = plt.figure()
+    ax = fig.add_axes([0.20, 0.20, 0.70, 0.70])
+    ax.plot(t,rk4solver(odefuncPP,X0,t,parameters))
+    plt.show()'''

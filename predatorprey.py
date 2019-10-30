d2a99ccf55e55db866ebeae6361c1ef8807286d4
maxoblein <max.oblein@gmail.com>
2019-10-30 12:03:13 +0000

    removed unused functions

--- predatorprey.py
+++ predatorprey.py
@@ -21,99 +21,10 @@ def odefuncPP(X,t,a,b,d):
     dXdt = [dx,dy]
     return np.array(dXdt)
 
-# Function to implement one step of the runge kutter fourth order method
-# X1 is the x at start of step and X2 is the end with respective t1 and t2
-def rk4(f,X1,t1,t2,parameters):
-    '''
-    function to implement the runge kutta fourth order integrator
-
-    inputs: -f ode Function
-            -X1 approx solution at start of timestep
-            -t1,t2 start and end of timestep
-            -parameters for ode
-
-    output: -X2 approx solution after timestep
-
-    '''
-
-    h = t2-t1
-    k1 = h*f(X1,t1,parameters)
-    k2 = h*f(X1+(k1/2),t1+(h/2),parameters)
-    k3 = h*f(X1+(k2/2),t1+(h/2),parameters)
-    k4 = h*f(X1+k3,t1+h,parameters)
-    X2 = X1 + ((1/6) * (k1+(2*k2)+(2*k3)+k4))
-    return X2
-
-def rk4solver(f,X0,t,parameters):
-    '''
-    function to implement rk4 over a time period
-
-    inputs: -f ode function
-            -X0 initial conditions
-            -t timeperiod
-            -parameters for ode
-
-    outputs: -the solutions after every timestep as an array
-
-    '''
-    #decide on a step size
-    h =  t[-1]/(np.size(t)-1)
-    t1 = t[0]
-    rk4_plot = np.array(X0)
-    X_rk4 = X0
-    #check that rk4 does not go pat the tend specified
-    while t1 < t[-1]:
-        tnext = min(t[-1],t1+h)
-        X_rk4 = rk4(f,X_rk4,t1,tnext,parameters)
-        rk4_plot = np.vstack((rk4_plot,X_rk4))
-        t1 = t1+h
-        t1 =round(t1,1)
-
-    return rk4_plot
-
-def find_period(sol_array):
-    '''
-    function to find the period of a given function
-
-    inputs: sol_array array of the solutions to the ode as found in integrator
-
-    outputs: the period of the given function
-    '''
-    peak_array, properties = signal.find_peaks(sol_array[:,0])
-
-    peak_times = []
-
-    for i in peak_array:
-        peak_times.append(t[i])
 
 
-    periods = []
 
-    for j in range(1,len(peak_times)):
-        periods.append(peak_times[j] - peak_times[j-1])
 
-    period_array = np.array(periods)
-    return np.mean(period_array)
-
-def sol_after_period(X0,f,t,parameters):
-    '''
-    function to find value after a calculated time period
-
-    inputs: -X0 initial conditions
-            -f ode function
-            -t timespan
-            -parameters for ode
-
-    outputs: -difference between inital conditions and the value after time period
-    '''
-    sol_array = rk4solver(f,X0,t,parameters)
-    period = find_period(sol_array)
-    index_of_period = np.argwhere(abs(t-period)< 0.05)
-    if np.size(index_of_period) != 1:
-        return [0,0]
-    index_of_period = index_of_period[0,0]
-
-    return abs(X0-sol_array[index_of_period,:])
 
 def sol_after_given_period(X0_T,f,t,parameters):
     '''given a set of initial conditions and a Time guessed to be the period
@@ -140,28 +51,7 @@ def sol_after_given_period(X0_T,f,t,parameters):
 
 
 
-def optimal_start(X0,t,parameters):
-    '''
-    function to find optimal initial conditions after calculating a time period
 
-    inputs: -X0 initial conditions
-            - t timespan
-            -parameters for ode
-
-    outputs: -plots solution for optimal starting conditions
-    '''
-    fig = plt.figure()
-    ax = fig.add_axes([0.20, 0.20, 0.70, 0.70])
-    solution = fsolve(sol_after_period,X0,(odefuncPP,t,parameters))
-    print(solution)
-
-    plot_array = rk4solver(odefuncPP,solution,t,parameters)
-    ax.plot(t,plot_array[:,0])
-    ax.plot(t,plot_array[:,1])
-    ax.hlines(solution[0],0,50)
-    ax.hlines(solution[1],0,50)
-    ax.vlines(18,0.25,0.35)
-    plt.show()
 
 def phase_condition(X0_T,f,t,parameters):
     '''

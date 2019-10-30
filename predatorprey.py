8d810d62dd7621db914603a3511b93ee6530b39c
maxoblein <max.oblein@gmail.com>
2019-10-30 11:59:20 +0000

    works assuming you use correct initial conditions

--- predatorprey.py
+++ predatorprey.py
@@ -1,5 +1,5 @@
 import sys
-from scipy.integrate import ode
+from scipy.integrate import odeint
 import scipy.signal as signal
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
@@ -7,15 +7,15 @@ import numpy as np
 from decimal import Decimal
 
 
-def odefuncPP(X,t,parameters):
+def odefuncPP(X,t,a,b,d):
     '''
         function to implement ode for predator prey system
 
     '''
     X.tolist()
-    a = parameters[0]
+    '''a = parameters[0]
     b = parameters[1]
-    d = parameters[2]
+    d = parameters[2]'''
     dx = X[0]*(1-X[0]) - ((a*X[0]*X[1])/(d+X[0]))
     dy = b * X[1] * (1 - (X[1]/X[0]))
     dXdt = [dx,dy]
@@ -130,7 +130,7 @@ def sol_after_given_period(X0_T,f,t,parameters):
 
     X0 = X0_T[0:2]
     period = X0_T[2]
-    sol_array = rk4solver(f,X0,t,parameters)
+    sol_array = odeint(f,X0,t,args = parameters)
     index_of_period = np.argwhere(abs(t-period)< 0.05)
     if np.size(index_of_period) != 1:
         return [0,0]
@@ -175,24 +175,24 @@ def phase_condition(X0_T,f,t,parameters):
     outputs: -phi constraints to be made to zero
     '''
     phi = np.zeros([3,1])
-    phi[0,0] = X0_T[0] - 0.32
-    phi[1,0] = (X0_T[0:2] - sol_after_given_period(X0_T,f,t,parameters))[0]
-    phi[2,0] = (X0_T[0:2] - sol_after_given_period(X0_T,f,t,parameters))[1]
+    phi[2,0] = X0_T[0] - 0.32
+    phi[0,0] = (X0_T[0:2] - sol_after_given_period(X0_T,f,t,parameters))[0]
+    phi[1,0] = (X0_T[0:2] - sol_after_given_period(X0_T,f,t,parameters))[1]
     phi = phi.flatten()
     return phi
 
 if __name__ == '__main__':
     #X0 = [0.3,0.3]
-    t = np.linspace(0,50,501)
+    t = np.linspace(0,500,5001)
 
     fig = plt.figure()
     ax = fig.add_axes([0.20, 0.20, 0.70, 0.70])
 
-    parameters = [1,0.26,0.1]
-    X0_T = np.array([0.4,0.4,18])
+    parameters = (1,0.26,0.1)
+    X0_T = np.array([0.32,0.28,20])
     solution = fsolve(phase_condition,X0_T,(odefuncPP,t,parameters))
     print(solution)
-    plot_array = rk4solver(odefuncPP,solution[0:2],t,parameters)
+    plot_array = odeint(odefuncPP,solution[0:2],t,args = parameters)
     ax.plot(t,plot_array[:,0])
     ax.plot(t,plot_array[:,1])
     ax.hlines(solution[0],0,50)
